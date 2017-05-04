7a4f65a85134beef56f883296da20874fdcad968
SAMI BETTAYEB <SAMI BETTAYEB>
2017-05-04 15:57:57 +0200

    1.0.0

---
+++ index.php
@@ -0,0 +1,73 @@
+﻿<html>
+<head>
+<title>Upload Service - By: SAMI BETTAYEB</title>
+<style type="text/css">
+*{
+	text-align: center;
+}
+#myform{
+	background-color: #CCCCCC;
+	width: 25%;
+	outline: double;
+}
+#fichier{
+	background-color: #0000FF;
+	color: #FFFFFF;
+}
+#titre,#description{
+	width: 90%;
+}
+#titre:focus,#description:focus{
+	width: 90%;
+	color:#FFFFFF;
+	background-color: #CC0000;
+	font-weight: bold;
+	outline: double 3px #FFFFFF;
+}
+</style>
+<script type="text/javascript" src='../JQuery/jquery-1.9.1.js'></script>
+<script type="text/javascript">
+/*function up(){
+	$.ajax({
+		type: 'REQUEST',
+		url: 'reception.php',
+		data: $('#myform').serialize(),
+		success: function(msg){
+			$('#show').html(msg);
+		}
+	});
+}*/
+$(document).ready(function(){
+	var i=0;
+	$('#ajout').click(function(){
+		i++;
+		$('#show_ajout').html($('#indice').attr('value'));
+		$('#show_ajout').prepend("<input type='file' name='monfichier"+i+"' /><br />");
+	});
+/*	$('#env').click(function(){
+		$.ajax({
+			url: 'reception.php',
+			type: 'POST',
+			data: ({'iiii':5}),
+			success:function(msg){
+				$('#show').html(msg);
+			}
+		});
+	});*/
+});
+</script>
+</head>
+
+<body>
+	<div id='show'></div>
+<form id="myform" action='reception.php' method='post' enctype='multipart/form-data'>
+<input type='hidden' name='MAX_FILE_SIZE' value='1048576' />
+<input type='hidden' name='indice' id='indice' value='0' />
+<input type='file' name='monfichier[]' id='fichier' multiple /><br />
+<div id="show_ajout"></div><input type='button' value='ajout' id='ajout' />
+Titre du fichier: <input type='text' name='titre' id='titre' /><br />
+Déscription du fichier: <textarea name='description' id='description' rows='4'></textarea><br />
+<input id='env' type='submit' value='Envoyer' onclick='up()' />
+</form>
+</body>
+</html>
---
+++ reception.php
@@ -0,0 +1,36 @@
+﻿<?php 
+
+for($i=0;$i<count($_FILES['monfichier']['name']);$i++){
+if($_FILES['monfichier']['error'][$i]==2){//echo("<script>alert('Erreur de taille.');</script>");
+echo "<h3>problème de taille!</h3>";
+//header('Location: index.php');
+}
+
+/*echo $_FILES['monfichier']['name'][$i]."<br />";
+echo $_FILES['monfichier']['type'][$i]."<br />";
+echo $_FILES['monfichier']['tmp_name'][$i]."<br />";
+echo $_FILES['monfichier']['size'][$i]."<br />";
+echo $_FILES['monfichier']['error'][$i]."<br />";*/
+$extensions_valides=array('jpg','png','gif','jpeg');
+if(!in_array(substr(strrchr(strtolower($_FILES['monfichier']['name'][$i]),'.'),1),$extensions_valides)){
+echo "<h1>Le fichier n'est pas une image!</h1>";
+}else{
+	$___id=md5(uniqid(rand(),true)).".".substr(strrchr(strtolower($_FILES['monfichier']['name'][$i]),'.'),1);
+	/*$dim=getimagesize($_FILES['monfichier']['tmp_name'][$i]);
+	$dim[0]='160';
+	$dim[1]='160';*/
+	$res=move_uploaded_file($_FILES['monfichier']['tmp_name'][$i], "./images/".$___id);
+	if($res){echo "Réussi"; echo "<img src='./images/".$___id."' width='50' />";}else{echo "Echoué";}
+}
+/*
+substr($string,$k); // ignore les $k premiers caractères de $string.
+strrchr($string,$cara); // renvoi la dernière partie de $string quand-on trouve le caractère $cara.
+strtolower($string); // mettre la chaîne de caractère $string en miniscules.
+in_array($val,$tab); // renvoi true, si la valeur $val est existe comme une valeur de $tab.
+$dim=getimagesize($image); // renvoi les dimensions d'image dans un tableau $dim tq: $dim[0] est la largeur(width), et $dim[1] est l'hauteur (height).
+*/
+/*echo "<h1>Le titre du fichier est: ".$_POST['titre']."</h1><br />";
+echo "<h1>La Déscription du fichier est: ".$_POST['description']."</h1>";*/
+}
+
+?>
